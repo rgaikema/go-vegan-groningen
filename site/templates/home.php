a99a31e7973ed017cc279173b3f1aec9a9945b79
Remco Gaikema <hallo@remcogaikema.nl>
2017-11-10 10:31:34 +0100

    added new logo's

--- site/templates/home.php
+++ site/templates/home.php
@@ -23,6 +23,7 @@
 
 		<section class="top">
 			<div class="logo desktop-only">
+
 				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 808 810" style="enable-background:new 0 0 808 810;" xml:space="preserve">
 					<style type="text/css">
 						.st0d{fill:url(#SVGID_1_);}
